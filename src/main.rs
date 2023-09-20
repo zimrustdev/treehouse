447a6edd5c2ad8eb562beaff739a7f5de16fb26f
Blessed Sibanda <zimrustdev@gmail.com>
2023-09-20 21:14:56 +0200

    Group visitors data with Structs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
@@ -11,16 +29,14 @@ fn what_is_your_name() -> String {
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    let visitor_list = ["bert", "steve", "fred"];
-    let mut allow_them_in = false;
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allow_them_in = true;
-        }
-    }
-    if allow_them_in {
-        println!("Welcome to the Treehouse, {}", name);
-    } else {
-        println!("Sorry, you aren't on the list.");
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+    match known_visitor {
+        Some(v) => v.greet_visitor(),
+        None => println!("You are not on the visitor list. Please leave."),
     }
 }
