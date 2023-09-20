9f61b419ac4b55c752030d87edee10ed6d51c733
Blessed Sibanda <zimrustdev@gmail.com>
2023-09-20 20:36:40 +0200

    Store visitor list in an Array

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,16 @@ fn what_is_your_name() -> String {
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {}", name);
+    let visitor_list = ["bert", "steve", "fred"];
+    let mut allow_them_in = false;
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true;
+        }
+    }
+    if allow_them_in {
+        println!("Welcome to the Treehouse, {}", name);
+    } else {
+        println!("Sorry, you aren't on the list.");
+    }
 }
